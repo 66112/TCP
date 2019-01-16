5a0d0735c341a52a7df541a18d0cf1c7a6a12d8d
66112 <1031802507@qq.com>
2019-01-16 19:40:16 +0800

    grad_child_work

---
+++ Grad_Fork_Tcp/server.cc
@@ -0,0 +1,12 @@
+#include "TcpGradFork.hpp"
+int main(int argc,char* argv[])
+{
+    if(argc != 2){
+        std::cerr << "argument error!" << " port" << std::endl;
+        exit(1);
+    }
+    Server* sp = new Server(atoi(argv[1]));
+    sp->ServerInit(5);
+    sp->ServerStart();
+    return 0;
+}
